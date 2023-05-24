4b689d50c8504132eaaedf726570c0358035314c
Zharfan0 <zharfanwidyadana@gmail.com>
2023-05-24 19:52:05 +0700

    vector dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -1,20 +1,36 @@
-// Asosiasi.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+using namespace std;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+class dokter;
+class pasien {
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) : nama(pNama) {
+        cout << "pasien \"" << nama << "\"ada\n";
+    }
+    ~pasien() {
+        cout << "pasien \"" << nama << "\"tidak akan \n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) : nama(pNama) {
+        cout << "dokter\"" << nama << "\"ada\n";
+    }
+    ~dokter() {
+        cout << "dokter\n" << nama << "\"tidak ada\n";
+    }
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
+
+

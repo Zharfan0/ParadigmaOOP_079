682b3d9c3a9c6ef0388eafcf6b2897e363fdf71d
Zharfan0 <zharfanwidyadana@gmail.com>
2023-05-24 19:44:57 +0700

    class pelajar string sekolah

--- SIswa/SIswa.cpp
+++ SIswa/SIswa.cpp
@@ -20,3 +20,18 @@ public:
         return a + b;
     }
 };
+
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) :
+        orang(pNama),
+        sekolah(pSekolah) {
+        cout << " pelajar dohapus\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+
+};
